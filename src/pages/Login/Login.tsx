29d422645fecfb8b2f3db3efda30734a48102dde
ck <70942824+ck-pp@users.noreply.github.com>
2025-02-04 11:57:13 +0900

    ✨ feat: 로그인/관리자 페이지 나누기

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,1 +1,12 @@
 /* src/pages/Login/Login.tsx */
+
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+function LoginPage() {
+    return (
+        <div className='login-container'></div>
+    );
+}
+
+export default LoginPage;
